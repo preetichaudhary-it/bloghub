d3f6fdd5461f04229b61a29d61aedbcbce8fc72d
Preeti Chaudhary <preetichaudhary20066@gmail.com>
2026-08-17 23:59:26 +0530

    Add Navbar & configure homepage routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')

20b3134301c525d9768bd240be381eb6404a5fdf
Preeti Chaudhary <preetichaudhary20066@gmail.com>
2026-09-07 14:33:47 +0530

    Implement user login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import (home, about, contact, register_view, profile)
+from .views import (home, about, contact, register_view, login_view, profile)
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
     path('profile/', profile, name='profile')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -33,6 +34,57 @@ def register_view(request):
         form = RegisterForm()
 
     return render(request, 'register.html',{'form':form})
+
+def login_view(request):
+    if request.user.is_authenticated:
+            return redirect('home')
+
+    # Default values for validation messages
+    username_error = None
+    password_error = None
+    login_error = None
+
+    # Keep username after an unsuccessful submission
+    username = ''
+
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip() # get values from html
+        password = request.POST.get('password', '')
+
+        # Username validation
+        if not username:
+            username_error = "Username is required."
+
+        # Password validation
+        if not password:
+            password_error = "Password is required"
+
+        # Authentication 
+        if not username_error and not password_error:
+            user = authenticate(
+                request,
+                username=username,
+                password=password
+            )
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome Back, {user.username}!")
+                return redirect('home')
+
+            else:
+                login_error = "Invalid username or password."
+
+    return render(
+        request,
+        'login.html',
+        {
+            'username': username,
+            'username_error': username_error,
+            'password_error': password_error,
+            'login_error': login_error,
+        }
+    )
 
 @login_required
 def profile(request):

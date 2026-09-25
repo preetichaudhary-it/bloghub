85764ec42af7ee95dbf0a45fe1dffb02f8e309d5
Preeti Chaudhary <preetichaudhary20066@gmail.com>
2026-09-25 10:46:48 +0530

    Add Category and BlogPost Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import BlogUser
+from .models import BlogUser, Category, BlogPost
+
 
 # Registering models here.
 @admin.register(BlogUser)
 class BlogUserAdmin(UserAdmin):
     pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'category',
+        'author',
+        'created_at'
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class BlogUser(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,3 +9,41 @@ class BlogUser(AbstractUser):
     def __str__(self):
         return self.username
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    image = models.ImageField(
+        upload_to='blog_images/',
+        blank=True,
+        null=True
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+    
+    def __str__(self):
+        return self.title
